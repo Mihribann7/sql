34df6dcbd5a86a1d799606413469e8fcb858f3a0
Mihribann7 <m.nosirova@newuu.uz>
2025-04-19 23:35:11 +0500

    Delete lesson-4/homework directory

--- lesson-4/homework/p3.sql
+++
@@ -1,4 +0,0 @@
-SELECT EmpId, EmpName, BirthDate
-FROM EmpBirth
-WHERE MONTH(BirthDate) = 5 
-  AND DAY(BirthDate) BETWEEN 7 AND 15;
--- lesson-4/homework/p4.sql
+++
@@ -1,5 +0,0 @@
-SELECT letter
-FROM letters
-ORDER BY 
-    CASE WHEN letter = 'b' THEN 0 ELSE 1 END, 
-    letter;
