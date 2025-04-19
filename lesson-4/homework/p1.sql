d664852ba29602a95c8f0b8703d3e153cdc7f991
Mihribann7 <m.nosirova@newuu.uz>
2025-04-19 23:28:25 +0500

    Add files via upload

---
+++ lesson-4/homework/p1.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM [dbo].[TestMultipleZero]
+WHERE NOT (ISNULL(A, 0) = 0 AND ISNULL(B, 0) = 0 AND ISNULL(C, 0) = 0 AND ISNULL(D, 0) = 0);
---
+++ lesson-4/homework/p3.sql
@@ -0,0 +1,4 @@
+SELECT EmpId, EmpName, BirthDate
+FROM EmpBirth
+WHERE MONTH(BirthDate) = 5 
+  AND DAY(BirthDate) BETWEEN 7 AND 15;
