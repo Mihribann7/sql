4ad4020c4073e265ae95786d242bf2cc65914c11
Mihribann7 <m.nosirova@newuu.uz>
2025-05-12 13:28:55 +0500

    Add files via upload

---
+++ lesson-8/homework/hw.sql
@@ -0,0 +1,91 @@
+--1
+
+DROP TABLE IF EXISTS Groupings;
+
+CREATE TABLE Groupings
+(
+StepNumber  INTEGER PRIMARY KEY,
+TestCase    VARCHAR(100) NOT NULL,
+[Status]    VARCHAR(100) NOT NULL
+);
+INSERT INTO Groupings (StepNumber, TestCase, [Status]) 
+VALUES
+(1,'Test Case 1','Passed'),
+(2,'Test Case 2','Passed'),
+(3,'Test Case 3','Passed'),
+(4,'Test Case 4','Passed'),
+(5,'Test Case 5','Failed'),
+(6,'Test Case 6','Failed'),
+(7,'Test Case 7','Failed'),
+(8,'Test Case 8','Failed'),
+(9,'Test Case 9','Failed'),
+(10,'Test Case 10','Passed'),
+(11,'Test Case 11','Passed'),
+(12,'Test Case 12','Passed');
+
+select 
+min(StepNumber) as minim,
+max(StepNumber) as maxim,
+status,
+count(*) as 'Consecutive Count'
+from (
+	select *,
+	ROW_NUMBER() over(order by stepNumber) - 
+	ROW_NUMBER() over( partition by status order by stepnumber) as groupID
+	from Groupings
+	) as sub
+group by status, groupID
+order by minim
+
+
+
+
+
+
+--2
+DROP TABLE IF EXISTS [dbo].[EMPLOYEES_N];
+
+CREATE TABLE [dbo].[EMPLOYEES_N]
+(
+    [EMPLOYEE_ID] [int] NOT NULL,
+    [FIRST_NAME] [varchar](20) NULL,
+    [HIRE_DATE] [date] NOT NULL
+)
+ 
+INSERT INTO [dbo].[EMPLOYEES_N]
+VALUES
+	(1001,'Pawan','1975-02-21'),
+	(1002,'Ramesh','1976-02-21'),
+	(1003,'Avtaar','1977-02-21'),
+	(1004,'Marank','1979-02-21'),
+	(1008,'Ganesh','1979-02-21'),
+	(1007,'Prem','1980-02-21'),
+	(1016,'Qaue','1975-02-21'),
+	(1155,'Rahil','1975-02-21'),
+	(1102,'Suresh','1975-02-21'),
+	(1103,'Tisha','1975-02-21'),
+	(1104,'Umesh','1972-02-21'),
+	(1024,'Veeru','1975-02-21'),
+	(1207,'Wahim','1974-02-21'),
+	(1046,'Xhera','1980-02-21'),
+	(1025,'Wasil','1975-02-21'),
+	(1052,'Xerra','1982-02-21'),
+	(1073,'Yash','1983-02-21'),
+	(1084,'Zahar','1984-02-21'),
+	(1094,'Queen','1985-02-21'),
+	(1027,'Ernst','1980-02-21'),
+	(1116,'Ashish','1990-02-21'),
+	(1225,'Bushan','1997-02-21');
+
+SELECT 
+  MIN(Year) AS StartYear,
+  MAX(Year) AS EndYear
+FROM (
+  SELECT 
+    DISTINCT YEAR(HIRE_DATE) AS Year,
+    ROW_NUMBER() OVER (ORDER BY YEAR(HIRE_DATE)) -
+    DENSE_RANK() OVER (ORDER BY YEAR(HIRE_DATE)) AS grp
+  FROM EMPLOYEES_N
+) AS Sub
+GROUP BY grp
+ORDER BY StartYear;
